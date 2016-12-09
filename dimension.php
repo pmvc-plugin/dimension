e0d9260124e782b3d73a70ee61e5d8d361f679e9
Hill <hill@kimo.com>
2016-12-09 13:23:38 +0000

    Add ENVIRONMENT support

--- dimension.php
+++ dimension.php
@@ -9,6 +9,8 @@ use PMVC\Event;
 
 ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\dimension';
 
+const QUERY = 'dimensionQuery';
+
 /**
  * @parameters string   dimensionUrl 
  * @parameters array    dimensionQuery
@@ -26,7 +28,7 @@ class dimension extends \PMVC\PlugIn
                 Event\MAP_REQUEST,
             ]
         );
-        $this['dimensionQuery'] = [];
+        $this[QUERY] = [];
     }
 
     public function onMapRequest($subject)
@@ -42,19 +44,23 @@ class dimension extends \PMVC\PlugIn
                 return false;
             }
         }
-        $this['dimensionQuery']['SITE']   = basename(\PMVC\getAppsParent());
-        $this['dimensionQuery']['APP']    = $c->getApp();
-        $this['dimensionQuery']['ACTION'] = $c->getAppAction();
+        $this[QUERY]['SITE']   = basename(\PMVC\getAppsParent());
+        $this[QUERY]['APP']    = $c->getApp();
+        $this[QUERY]['ACTION'] = $c->getAppAction();
+        $env = \PMVC\plug('getenv')->get('ENVIRONMENT');
+        if ($env) {
+            $this[QUERY]['ENVIRONMENT'] = $env;
+        }
         $entry = explode(
             '.',
             \PMVC\plug('url')->getRunPhp()
         );
-        $this['dimensionQuery']['ENTRY']=$entry[0];
+        $this[QUERY]['ENTRY']=$entry[0];
         if (isset($this['getDimension'])) {
             call_user_func_array(
                 $this['getDimension'],
                 [
-                    &$this['dimensionQuery'],
+                    &$this[QUERY],
                     $c->getRequest() 
                 ]
             );
@@ -67,13 +73,18 @@ class dimension extends \PMVC\PlugIn
     {
         $url = \PMVC\plug('url')
             ->getUrl($this['dimensionUrl']);
-        $url->query = $this['dimensionQuery'];
+        $url->query = $this[QUERY];
         $curl = \PMVC\plug('curl');
         $curl->get($url, function($r){
             $json = \PMVC\fromJson($r->body, true); 
             if (is_array($json)) {
                 $this->unsetCli($json);
-                \PMVC\dev(function() use ($json){return $json;},'dimension');
+                \PMVC\dev(function() use ($json){
+                    return [
+                        'query'=>$this[QUERY],
+                        'configs'=>$json
+                    ];
+                },'dimension');
                 \PMVC\option('set', $json);
             } else {
                 $dot = \PMVC\plug('dotenv');
